511b6e58248d0c2fadfe1ee8a74a7c360eb59445
wagnerpires <wagnerpires@hotmail.com>
2023-05-30 04:26:46 -0300

    Angular theme

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-
 @NgModule({
   declarations: [
     AppComponent
